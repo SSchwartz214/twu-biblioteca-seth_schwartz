d376b83fd38a529340780670098333cbe30b67ff
Seth Schwartz <sethschwartz@Seths-MacBook-Pro.local>
2019-04-12 14:24:36 -0500

    Revises test for booklist

--- src/com/twu/biblioteca/BookList.java
+++ src/com/twu/biblioteca/BookList.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 
 
 public class BookList {
-    private ArrayList<Book> bookList = new ArrayList<Book>();
+    private ArrayList<Book> list = new ArrayList<Book>();
 
     public ArrayList<Book> getBookList() {
-        bookList.add( new Book("Animal Farm", "George Orwell", 1945) );
-        bookList.add( new Book("The Giver", "Louis Lowryl", 1993) );
-        bookList.add( new Book("Fifty Shades of Grey", "E. L. James", 2011) );
+        list.add( new Book("Animal Farm", "George Orwell", 1945) );
+        list.add( new Book("The Giver", "Louis Lowryl", 1993) );
+        list.add( new Book("Fifty Shades of Grey", "E. L. James", 2011) );
 
-        return bookList;
+        return list;
     }
 }
--- test/com/twu/biblioteca/BookListTest.java
+++ test/com/twu/biblioteca/BookListTest.java
@@ -1,22 +1,24 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
-import java.util.ArrayList;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.Matchers.*;
 
 public class BookListTest {
 
     @Test
     public void shouldReturnListOfBooks() {
         BookList bookList = new BookList();
-        ArrayList<Book> bookList = new ArrayList<Book>();
-        bookList.add( new Book("Animal Farm", "George Orwell", 1945) );
-        bookList.add( new Book("The Giver", "Louis Lowryl", 1993) );
-        bookList.add( new Book("Fifty Shades of Grey", "E. L. James", 2011) );
+        assertThat((bookList.getBookList()), hasSize(3));
 
-        assertThat(bookList.getBookList(), is(books));
-        assertThat(books, hasSize(3));
+        String str1 = bookList.getBookList().get(0).toString();
+        assertThat(str1, hasToString(str1));
+
+        String str2 = bookList.getBookList().get(1).toString();
+        assertThat(str2, hasToString(str2));
+
+        String str3 = bookList.getBookList().get(2).toString();
+        assertThat(str3, hasToString(str3));
     }
 }
