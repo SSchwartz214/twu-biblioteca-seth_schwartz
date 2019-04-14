003dab3cdcdde26c255a239890afa7590727257e
Seth Schwartz <sethschwartz@Seths-MacBook-Pro.local>
2019-04-14 16:57:51 -0500

    Completes 1.6

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,12 +3,8 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        //TODO refactor into Menu class
-        WelcomeMessage welcomeMessage = new WelcomeMessage();
         Menu menu = new Menu();
 
-        System.out.println(welcomeMessage.printWelcomeMessage());
         menu.run();
-//        System.out.println(bookList.printLibrary(bookList.getLibrary()));
     }
 }
--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -25,6 +25,4 @@ public class MenuTest {
         //then
         assertThat(result2, is(false));
     }
-
-
 }
