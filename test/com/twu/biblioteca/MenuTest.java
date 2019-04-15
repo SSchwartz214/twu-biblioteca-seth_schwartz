f75cb17ce7e3087191d574a6f33ea2ccd288b1ec
Seth Schwartz <sethschwartz@Seths-MacBook-Pro.local>
2019-04-15 13:48:05 -0500

    Makes all tests pass

--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -8,21 +8,34 @@ public class MenuTest {
 
     @Test
     public void shouldValidateOptionsTest() {
-        //Given
         Menu menu = new Menu();
         String userOption1 = "1";
         String userOption2 = "2";
+        String userOption3 = "3";
+        String userOption4 = "4";
+        String userOption5 = "5";
 
-        //When
         boolean result1 = menu.validateOption(userOption1);
 
-        //Then
         assertThat(result1, is(true));
 
-        //when
+
         boolean result2 = menu.validateOption(userOption2);
 
-        //then
-        assertThat(result2, is(false));
+        assertThat(result2, is(true));//when
+
+
+        boolean result3 = menu.validateOption(userOption3);
+
+        assertThat(result3, is(true));
+
+
+        boolean result4 = menu.validateOption(userOption4);
+
+        assertThat(result4, is(true));
+
+        boolean result5 = menu.validateOption(userOption5);
+
+        assertThat(result5, is(false));
     }
 }
